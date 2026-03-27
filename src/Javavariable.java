5bfaa7d63bac258e105a90ab87fb2d179182406d
sanjaysd636-coder <sanjaysd636@gmail.com>
2026-03-27 18:27:37 +0530

    java variable updated

---
+++ src/Javavariable.java
@@ -0,0 +1,15 @@
+public class Javavariable {
+    public static void main(String[] args) {
+
+        int age = 25;
+        String name = "Sanjay";
+        double salary = 50000.50;
+        boolean isDeveloper = true;
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(salary);
+        System.out.println(isDeveloper);
+    }
+}
+
