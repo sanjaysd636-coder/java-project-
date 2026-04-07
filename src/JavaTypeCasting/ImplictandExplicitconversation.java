d542462b42683e86a138e2f1d424cb3b8f2ec6a3
sanjaysd636-coder <sanjaysd636@gmail.com>
2026-04-07 23:26:36 +0530

    JavaTypecasting updated

---
+++ src/JavaTypeCasting/ImplictandExplicitconversation.java
@@ -0,0 +1,16 @@
+package JavaTypeCasting;
+
+public class ImplictandExplicitconversation {
+    public static void main(String[] args) {
+
+        int a = 100;
+        double result = a;
+        System.out.println("result====>" + result);
+
+        double value = 1000;
+        float floatResult = (float) value;
+        System.out.println("float Result====>" + floatResult);
+
+    }
+}
+
